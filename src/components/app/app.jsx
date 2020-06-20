7d38bca6452fc10902f6c6cadb31544b8fae5119
Nefila <veronika.sts@gmail.com>
2020-06-20 22:46:46 +0200

    Подключает роутер

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,32 +1,42 @@
-import React from 'react';
-import Main from '../main/main.jsx';
+import React, {PureComponent} from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import PropTypes from "prop-types";
+import Main from '../main/main.jsx';
+import MoviePage from '../movie-page/movie-page.jsx';
 
 
 const movieTitleHandler = () => { };
 
-const App = ({movie, movies}) => {
-  return (
-    <Main
-      movie={movie}
-      movies={movies}
-      onMovieTitleClick={movieTitleHandler}
-    />
-  );
-};
+export default class App extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const {promoMovie, movies} = this.props;
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Main
+              promoMovie={promoMovie}
+              movies={movies}
+              onMovieTitleClick={movieTitleHandler}
+            />
+          </Route>
+          <Route exact path="/movie">
+            <MoviePage
+              movies={movies}
+              onMovieTitleClick={movieTitleHandler}
+            />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
 
 App.propTypes = {
-  movie: PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    genre: PropTypes.string.isRequired,
-    releaseDate: PropTypes.number.isRequired,
-  }).isRequired,
-  movies: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        title: PropTypes.string.isRequired,
-        url: PropTypes.string.isRequired,
-      })).isRequired,
+  promoMovie: PropTypes.object.isRequired,
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
-
-export default App;
