84679d68d4462e81e20cb30e15dd60a09ecf16c1
Nefila <veronika.sts@gmail.com>
2020-06-24 13:48:22 +0200

    Добавляет снепшот тест для компонента MoviePage

--- src/components/movie-card/movie-card.test.js
+++ src/components/movie-card/movie-card.test.js
@@ -1,5 +1,5 @@
-import React from "react";
-import renderer from "react-test-renderer";
+import React from 'react';
+import renderer from 'react-test-renderer';
 import MovieCard from './movie-card.jsx';
 
 const movie = {
