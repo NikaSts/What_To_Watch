19c114344f7adf37a1e5b5ee4efe1d8adab884bb
Nefila <veronika.sts@gmail.com>
2020-07-01 14:26:28 +0200

    Создает GenreList

---
+++ src/components/genre-list/genre-list.jsx
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import {arrayOf, string} from 'prop-types';
+
+export default class GenreList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: `All genres`,
+    };
+
+    this.handleListItemClick = this.handleListItemClick.bind(this);
+  }
+
+  handleListItemClick(evt) {
+    evt.preventDefault();
+    const target = evt.target;
+    this.setState({active: target.textContent});
+  }
+
+  render() {
+    const {genres} = this.props;
+    return (
+      <ul className="catalog__genres-list">
+        {genres.map((genre) => {
+          const {active} = this.state;
+          const activeClass = genre !== active || `catalog__genres-item--active`;
+          return (
+            <li
+              key={genre}
+              className={`catalog__genres-item ${activeClass}`}>
+              <a
+                href="#"
+                className="catalog__genres-link"
+                onClick={this.handleListItemClick}>
+                {genre}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
+}
+
+GenreList.propTypes = {
+  genres: arrayOf(string.isRequired).isRequired,
+};
---
+++ src/components/genre-list/genre-list.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import GenreList from './genre-list.jsx';
+
+const genres = [`All genres`, `Comedies`, `Crime`, `Documentary`, `Dramas`, `Horror`, `Kids & Family`, `Romance`, `Sci-Fi`, `Thrillers`];
+
+
+it(`MoviesList should render correctly`, () => {
+  const tree = renderer
+      .create(<GenreList
+        genres={genres}
+      />)
+      .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
