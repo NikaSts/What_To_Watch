d284b9fb10b61fb95d587acabfa46f387a96be0e
Nefila <veronika.sts@gmail.com>
2020-07-24 13:27:34 +0200

    Добавит модуль работы с API

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import {Provider} from 'react-redux';
+import {thunk} from 'redux-thunk';
+
 import App from './components/app/app';
 import {rootReducer} from './store/reduсers/reducer';
+import {createAPI} from './api';
 
+import {requireAuthorization} from './store/actions';
+import {AuthorizationStatus} from './utils/consts';
+
+const api = createAPI(() => {
+  store.dispatch(requireAuthorization(AuthorizationStatus.NO_AUTH));
+});
 
 const store = createStore(
     rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+    compose(
+        applyMiddleware(thunk.withExtraArgument(api)),
+        window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+    )
 );
 
 ReactDOM.render(
